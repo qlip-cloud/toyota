29d058bf88c80d4ddd5ab814c9d34e77c212c226
CaMentum <camilo.otalora@mentum.group>
2023-09-14 05:44:31 +0000

    Creacion de 2 doctype nuevos

---
+++ toyota/toyota/doctype/qp_tyt_solicitudes_de_credito/test_qp_tyt_solicitudes_de_credito.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2023, Mentum and Contributors
+# See license.txt
+
+# import frappe
+import unittest
+
+class Testqp_TYT_solicitudes_de_credito(unittest.TestCase):
+	pass
---
+++ toyota/toyota/doctype/qp_tyt_telefono_a_caso/qp_tyt_telefono_a_caso.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, Mentum and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('qp_TYT_telefono_a_caso', {
+	// refresh: function(frm) {
+
+	// }
+});
